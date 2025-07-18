5725b3fdee54fefa1f9fc1070ee56e1e6e4e0a9c
jhonathan Silva <105667004+JhonathanSilva2@users.noreply.github.com>
2025-07-18 14:30:28 -0300

    chore: modification for testing CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Welcome to Macaé Tempo!</h1>
+      <h1 className="text-4xl font-bold">Welcome to Macaé Tempo!XXXXXXXXX</h1>
       <p className="mt-4">This is a simple starter template.</p>
     </div>
   )
